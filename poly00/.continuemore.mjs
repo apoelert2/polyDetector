7f10d9c8286eb4ae93005bec688b8340d20bf404
apoelert2 <ktm_heizzer@gmx.de>
2026-05-18 08:08:28 +0200

    fix(poller,extractor): add outcome prices to entities, clean up formatting

--- poly00/.continuemore.mjs
+++
@@ -1,9 +0,0 @@
-// .continuemore.mjs
-export function modifyConfig(config) {
-  config.contextProvider = config.contextProvider || {};
-  config.contextProvider.maxTokensFromHistory = 8000;
-  config.defaultCompletionOptions = config.defaultCompletionOptions || {};
-  config.defaultCompletionOptions.maxTokens = 1024;
-  config.defaultCompletionOptions.temperature = 0.2;
-  return config;
-}
